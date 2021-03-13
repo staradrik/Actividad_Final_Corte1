c11ca1476163ac3390de96216ba2c14b42c61dfa
LUZ ADRIANA YARA TIQUE <adrianayara12@gmail.com>
2021-03-13 13:21:36 -0500

    Agregar archivos de proyecto.

---
+++ Actividad_Final_Corte1/ContenidoModulos.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Actividad_Final_Corte1
+{
+    partial class ContenidoModulos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_modulo1 = new System.Windows.Forms.Button();
+            this.btn_modulo2 = new System.Windows.Forms.Button();
+            this.btn_modulo3 = new System.Windows.Forms.Button();
+            this.btn_modulo4 = new System.Windows.Forms.Button();
+            this.btn_modulo5 = new System.Windows.Forms.Button();
+            this.btn_modulo6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_modulo1
+            // 
+            this.btn_modulo1.Location = new System.Drawing.Point(123, 38);
+            this.btn_modulo1.Name = "btn_modulo1";
+            this.btn_modulo1.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo1.TabIndex = 0;
+            this.btn_modulo1.Text = "Modulo 1";
+            this.btn_modulo1.UseVisualStyleBackColor = true;
+            // 
+            // btn_modulo2
+            // 
+            this.btn_modulo2.Location = new System.Drawing.Point(123, 68);
+            this.btn_modulo2.Name = "btn_modulo2";
+            this.btn_modulo2.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo2.TabIndex = 1;
+            this.btn_modulo2.Text = "Modulo 2";
+            this.btn_modulo2.UseVisualStyleBackColor = true;
+            // 
+            // btn_modulo3
+            // 
+            this.btn_modulo3.Location = new System.Drawing.Point(123, 97);
+            this.btn_modulo3.Name = "btn_modulo3";
+            this.btn_modulo3.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo3.TabIndex = 2;
+            this.btn_modulo3.Text = "Modulo 3";
+            this.btn_modulo3.UseVisualStyleBackColor = true;
+            // 
+            // btn_modulo4
+            // 
+            this.btn_modulo4.Location = new System.Drawing.Point(123, 126);
+            this.btn_modulo4.Name = "btn_modulo4";
+            this.btn_modulo4.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo4.TabIndex = 3;
+            this.btn_modulo4.Text = "Modulo 4";
+            this.btn_modulo4.UseVisualStyleBackColor = true;
+            // 
+            // btn_modulo5
+            // 
+            this.btn_modulo5.Location = new System.Drawing.Point(123, 155);
+            this.btn_modulo5.Name = "btn_modulo5";
+            this.btn_modulo5.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo5.TabIndex = 4;
+            this.btn_modulo5.Text = "Modulo 5";
+            this.btn_modulo5.UseVisualStyleBackColor = true;
+            // 
+            // btn_modulo6
+            // 
+            this.btn_modulo6.Location = new System.Drawing.Point(123, 184);
+            this.btn_modulo6.Name = "btn_modulo6";
+            this.btn_modulo6.Size = new System.Drawing.Size(75, 23);
+            this.btn_modulo6.TabIndex = 5;
+            this.btn_modulo6.Text = "Modulo 6";
+            this.btn_modulo6.UseVisualStyleBackColor = true;
+            // 
+            // ContenidoModulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 245);
+            this.Controls.Add(this.btn_modulo6);
+            this.Controls.Add(this.btn_modulo5);
+            this.Controls.Add(this.btn_modulo4);
+            this.Controls.Add(this.btn_modulo3);
+            this.Controls.Add(this.btn_modulo2);
+            this.Controls.Add(this.btn_modulo1);
+            this.Name = "ContenidoModulos";
+            this.Text = "CONTENIDO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_modulo1;
+        private System.Windows.Forms.Button btn_modulo2;
+        private System.Windows.Forms.Button btn_modulo3;
+        private System.Windows.Forms.Button btn_modulo4;
+        private System.Windows.Forms.Button btn_modulo5;
+        private System.Windows.Forms.Button btn_modulo6;
+    }
+}
+

660d6105276f5cdff6cb9dab2effcf8ae91fa7ce
LUZ ADRIANA YARA TIQUE <adrianayara12@gmail.com>
2021-03-16 09:42:58 -0500

    Modulo 1

---
+++ Actividad_Final_Corte1/Modulo1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Actividad_Final_Corte1
+{
+    public partial class Modulo1 : Form
+    {
+        public Modulo1()
+        {
+            InitializeComponent();
+        }
+    }
+}

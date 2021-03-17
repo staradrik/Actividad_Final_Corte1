353cd5df2b7c7d2583bd743213337095a7044365
LUZ ADRIANA YARA TIQUE <adrianayara12@gmail.com>
2021-03-17 11:48:33 -0500

    diseño 3

--- Actividad_Final_Corte1/Modulo3.Designer.cs
+++ Actividad_Final_Corte1/Modulo3.Designer.cs
@@ -29,19 +29,143 @@ namespace Actividad_Final_Corte1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Modulo3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(84, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Palabra o Frase";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(84, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Letra a contar";
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(197, 65);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(263, 23);
+            this.txtFrase.TabIndex = 2;
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(197, 109);
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(77, 23);
+            this.txtLetra.TabIndex = 3;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegresar.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRegresar.Location = new System.Drawing.Point(250, 240);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 4;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnlimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnlimpiar.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnlimpiar.Location = new System.Drawing.Point(298, 154);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpiar.TabIndex = 5;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnContar
+            // 
+            this.btnContar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnContar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnContar.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnContar.Location = new System.Drawing.Point(199, 154);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 6;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = false;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(91, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(197, 201);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(77, 23);
+            this.txtResultado.TabIndex = 8;
             // 
             // Modulo3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(249, 178);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(472, 311);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.txtLetra);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Modulo3";
             this.Text = "Modulo3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.TextBox txtLetra;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
--- Actividad_Final_Corte1/Modulo3.cs
+++ Actividad_Final_Corte1/Modulo3.cs
@@ -14,5 +14,27 @@ namespace Actividad_Final_Corte1
         {
             InitializeComponent();
         }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            String cadena = txtFrase.Text;
+            int ocurrencias = 0;
+            ocurrencias = cadena.Split(txtLetra.Text).Length - 1;
+            txtResultado.Text = Convert.ToString(ocurrencias);
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtFrase.Clear();
+            txtLetra.Clear();
+            txtResultado.Clear();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            ContenidoModulos Contemd = new ContenidoModulos();
+            this.Hide();
+            Contemd.Show();
+        }
     }
 }

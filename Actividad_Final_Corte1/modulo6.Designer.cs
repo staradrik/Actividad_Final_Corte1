e8d4fb784f8f77df345a0443f6fd5d6b088fa96a
dayanapenagosa@gmail.com <dayanapenagosa@gmail.com>
2021-03-18 05:33:20 -0500

    Modulo6

---
+++ Actividad_Final_Corte1/modulo6.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Actividad_Final_Corte1
+{
+    partial class modulo6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(modulo6));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.txtRESUL = new System.Windows.Forms.TextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnLIMPIAR = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(154, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Una suma dificil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(55, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Números a sumar:";
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(194, 81);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(171, 22);
+            this.txtNumeros.TabIndex = 2;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnSumar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSumar.Location = new System.Drawing.Point(194, 134);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 3;
+            this.btnSumar.Text = "SUMAR";
+            this.btnSumar.UseVisualStyleBackColor = false;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // txtRESUL
+            // 
+            this.txtRESUL.Location = new System.Drawing.Point(194, 194);
+            this.txtRESUL.Name = "txtRESUL";
+            this.txtRESUL.Size = new System.Drawing.Size(126, 22);
+            this.txtRESUL.TabIndex = 4;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegresar.Location = new System.Drawing.Point(104, 279);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 5;
+            this.btnRegresar.Text = "REGRESAR";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // btnLIMPIAR
+            // 
+            this.btnLIMPIAR.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnLIMPIAR.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLIMPIAR.Location = new System.Drawing.Point(267, 279);
+            this.btnLIMPIAR.Name = "btnLIMPIAR";
+            this.btnLIMPIAR.Size = new System.Drawing.Size(75, 23);
+            this.btnLIMPIAR.TabIndex = 6;
+            this.btnLIMPIAR.Text = "LIMPIAR";
+            this.btnLIMPIAR.UseVisualStyleBackColor = false;
+            this.btnLIMPIAR.Click += new System.EventHandler(this.btnLIMPIAR_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(55, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Su resultado es:";
+            // 
+            // modulo6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(418, 358);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLIMPIAR);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.txtRESUL);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "modulo6";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.modulo6_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.TextBox txtRESUL;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnLIMPIAR;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Actividad_Final_Corte1/modulo6.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Actividad_Final_Corte1
+{
+    public partial class modulo6 : Form
+    {
+        public modulo6()
+        {
+            InitializeComponent();
+        }
+
+        private void modulo6_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            Double sum = 0;
+            String numeros;
+            numeros = txtNumeros.Text;
+
+            string[] listanumeros = numeros.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                for (int i = 0; i < listanumeros.Length; i++)
+                {
+                    sum += Double.Parse(listanumeros[i]);
+                }
+                txtRESUL.Text = ("" + sum);
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void btnLIMPIAR_Click(object sender, EventArgs e)
+        {
+            txtNumeros.Clear();
+            txtRESUL.Clear();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            //   ContenidoModulos CModulos = new ContenidoModulos();
+            //  this.Hide();
+            //CModulos.Show();
+        }
+    }
+}

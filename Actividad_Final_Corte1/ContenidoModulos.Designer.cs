4016ff7e7aaa55f3a25a95a73d7c43d8b37d3534
LUZ ADRIANA YARA TIQUE <adrianayara12@gmail.com>
2021-03-18 13:56:16 -0500

    documentación

---
+++ Actividad_Final_Corte1/ContenidoModulos.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Actividad_Final_Corte1
+{
+    partial class ContenidoModulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContenidoModulos));
+            this.bt_m1 = new System.Windows.Forms.Button();
+            this.btn_m2 = new System.Windows.Forms.Button();
+            this.btn_m3 = new System.Windows.Forms.Button();
+            this.btn_m6 = new System.Windows.Forms.Button();
+            this.btn_m5 = new System.Windows.Forms.Button();
+            this.btn_m4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_m1
+            // 
+            this.bt_m1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.bt_m1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_m1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bt_m1.Location = new System.Drawing.Point(137, 74);
+            this.bt_m1.Name = "bt_m1";
+            this.bt_m1.Size = new System.Drawing.Size(85, 36);
+            this.bt_m1.TabIndex = 0;
+            this.bt_m1.Text = "MODULO 1";
+            this.bt_m1.UseVisualStyleBackColor = false;
+            this.bt_m1.Click += new System.EventHandler(this.bt_m1_Click);
+            // 
+            // btn_m2
+            // 
+            this.btn_m2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_m2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_m2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_m2.Location = new System.Drawing.Point(228, 74);
+            this.btn_m2.Name = "btn_m2";
+            this.btn_m2.Size = new System.Drawing.Size(91, 36);
+            this.btn_m2.TabIndex = 1;
+            this.btn_m2.Text = "MODULO 2";
+            this.btn_m2.UseVisualStyleBackColor = false;
+            this.btn_m2.Click += new System.EventHandler(this.btn_m2_Click);
+            // 
+            // btn_m3
+            // 
+            this.btn_m3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_m3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_m3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_m3.Location = new System.Drawing.Point(137, 124);
+            this.btn_m3.Name = "btn_m3";
+            this.btn_m3.Size = new System.Drawing.Size(85, 36);
+            this.btn_m3.TabIndex = 2;
+            this.btn_m3.Text = "MODULO 3";
+            this.btn_m3.UseVisualStyleBackColor = false;
+            this.btn_m3.Click += new System.EventHandler(this.btn_m3_Click);
+            // 
+            // btn_m6
+            // 
+            this.btn_m6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn_m6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_m6.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_m6.Location = new System.Drawing.Point(228, 177);
+            this.btn_m6.Name = "btn_m6";
+            this.btn_m6.Size = new System.Drawing.Size(91, 36);
+            this.btn_m6.TabIndex = 3;
+            this.btn_m6.Text = "MODULO 6";
+            this.btn_m6.UseVisualStyleBackColor = false;
+            this.btn_m6.Click += new System.EventHandler(this.btn_m6_Click);
+            // 
+            // btn_m5
+            // 
+            this.btn_m5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_m5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_m5.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_m5.Location = new System.Drawing.Point(137, 177);
+            this.btn_m5.Name = "btn_m5";
+            this.btn_m5.Size = new System.Drawing.Size(85, 36);
+            this.btn_m5.TabIndex = 4;
+            this.btn_m5.Text = "MODULO 5";
+            this.btn_m5.UseVisualStyleBackColor = false;
+            this.btn_m5.Click += new System.EventHandler(this.btn_m5_Click);
+            // 
+            // btn_m4
+            // 
+            this.btn_m4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_m4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_m4.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_m4.Location = new System.Drawing.Point(228, 124);
+            this.btn_m4.Name = "btn_m4";
+            this.btn_m4.Size = new System.Drawing.Size(91, 36);
+            this.btn_m4.TabIndex = 5;
+            this.btn_m4.Text = "MODULO 4";
+            this.btn_m4.UseVisualStyleBackColor = false;
+            this.btn_m4.Click += new System.EventHandler(this.btn_m4_Click);
+            // 
+            // ContenidoModulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(576, 303);
+            this.Controls.Add(this.btn_m4);
+            this.Controls.Add(this.btn_m5);
+            this.Controls.Add(this.btn_m6);
+            this.Controls.Add(this.btn_m3);
+            this.Controls.Add(this.btn_m2);
+            this.Controls.Add(this.bt_m1);
+            this.Name = "ContenidoModulos";
+            this.Text = "ContenidoModulos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_m1;
+        private System.Windows.Forms.Button btn_m2;
+        private System.Windows.Forms.Button btn_m3;
+        private System.Windows.Forms.Button btn_m6;
+        private System.Windows.Forms.Button btn_m5;
+        private System.Windows.Forms.Button btn_m4;
+    }
+}
---
+++ Actividad_Final_Corte1/ContenidoModulos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Actividad_Final_Corte1
+{
+    public partial class ContenidoModulos : Form
+    {
+        public ContenidoModulos()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_m1_Click(object sender, EventArgs e)
+        {
+            Modulo1 m1 = new Modulo1();
+            this.Hide();
+            m1.Show();
+        }
+
+        private void btn_m2_Click(object sender, EventArgs e)
+        {
+            Modulo2 m2 = new Modulo2();
+            this.Hide();
+            m2.Show();
+        }
+
+        private void btn_m3_Click(object sender, EventArgs e)
+        {
+            Modulo3 m3 = new Modulo3();
+            this.Hide();
+            m3.Show();
+        }
+
+        private void btn_m4_Click(object sender, EventArgs e)
+        {
+            modulo4 m4 = new modulo4();
+            this.Hide();
+            m4.Show();
+        }
+
+        private void btn_m5_Click(object sender, EventArgs e)
+        {
+            Modulo5 m5 = new Modulo5();
+            this.Hide();
+            m5.Show();
+        }
+
+        private void btn_m6_Click(object sender, EventArgs e)
+        {
+            modulo6 m6 = new modulo6();
+            this.Hide();
+            m6.Show();
+        }
+    }
+}

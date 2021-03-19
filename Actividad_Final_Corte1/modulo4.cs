9e4e2de82a079209e947d0e5f77339483bb21c39
dayanapenagosa@gmail.com <dayanapenagosa@gmail.com>
2021-03-18 21:56:59 -0500

    Documentacion - modulo4

--- Actividad_Final_Corte1/modulo4.cs
+++ Actividad_Final_Corte1/modulo4.cs
@@ -19,13 +19,17 @@ namespace Actividad_Final_Corte1
         {
             
 
-                String Palabra;
+                String Palabra; //Variable String - CADENA DE CARACTERES
                 Palabra = txtPalabra.Text;
-            for (int x = Palabra.Length - 1; x >= 0; x--) {
-                String Resul;
-                Resul = txtResultado.Text;
-                Resul += Palabra[x];
 
+            //CICLO FOR QUE RECORRE LA CADENA - LENGTH DETERMMINA EL TAMAÑO DE LA CADENA 
+            for (int x = Palabra.Length - 1; x >= 0; x--) { 
+
+                String Resul;  //Variable String - CADENA RESULTANTE DE CARACTERES
+                Resul = txtResultado.Text;
+                //SE AGREGA CARACTER POR CARACTER AL RESULTADO, INCIANDO DESDE EL ULTIMO CARACTER
+                Resul += Palabra[x];
+                //ASIGNA EL RESULTADO
                 txtResultado.Text = (Resul);
 
 
@@ -35,12 +39,14 @@ namespace Actividad_Final_Corte1
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
+            //Limpiar cajas de texto
             txtPalabra.Clear();
             txtResultado.Clear();
         }
 
         private void btnRegresar_Click(object sender, EventArgs e)
         {
+            //Regresar
              ContenidoModulos CModulos = new ContenidoModulos();
               this.Hide();
             CModulos.Show();
